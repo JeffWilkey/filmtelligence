d0669167ace68303f6d148ea22882957f63a75b5
Jeff Wilkey <jeffrotull92@gmail.com>
2018-03-14 09:50:41 -0700

    build search functionality on movie page

--- js/app.js
+++ js/app.js
@@ -3,6 +3,18 @@ const TMDB_MOVIE_URL = 'https://api.themoviedb.org/3/movie/'
 const API_KEY = "78cb36c6b59ca43ebda01b258e8ff0d1"
 let currentMovie = {}
 
+// Search and API Functionality
+function watchSubmit() {
+    $('.js-search').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+
+        getDataFromApi(query, getMovie);
+    });
+}
+
+// Get Initial Search Results
 function getDataFromApi(searchTerm, callback) {
     const query = {
         api_key: API_KEY,
@@ -12,6 +24,7 @@ function getDataFromApi(searchTerm, callback) {
     $.getJSON(TMDB_SEARCH_URL, query, callback);
 }
 
+// Get the movie object
 function getMovieFromApi(movieId, callback) {
   let movie = {};
   const query = {
@@ -19,6 +32,11 @@ function getMovieFromApi(movieId, callback) {
   }
   $.getJSON(TMDB_MOVIE_URL + movieId, query, callback );
 
+}
+
+// Return the movie object to the render function
+function getMovie(result) {
+  getMovieFromApi(result.results[0].id, returnAndRenderMovie)
 }
 
 
@@ -42,25 +60,7 @@ function renderFrontPage() {
   `)
 }
 
-function watchSubmit() {
-    $('.js-search').submit(event => {
-        event.preventDefault();
-        const queryTarget = $(event.currentTarget).find('.js-query');
-        const query = queryTarget.val();
-
-        getDataFromApi(query, getMovie);
-    });
-}
-
-// Get The Movie Object
-function getMovie(result) {
-  console.log(result)
-  getMovieFromApi(result.results[0].id, returnAndRenderMovie)
-}
-
-// Render the movie object
-function returnAndRenderMovie(result) {
-  console.log(result);
+function removeFrontPage() {
   $(".header-logo-text").fadeOut(400, function() {
     $(this).remove();
   });
@@ -69,56 +69,77 @@ function returnAndRenderMovie(result) {
   });
   $(".js-search").fadeOut(400, function() {
     $(this).remove();
-  })
-  var logoStyles = {
-    width: "50px",
-    height: "50px",
-    marginTop: "10px"
-  }
-  $(".header-logo").css(logoStyles);
+  });
   $(".container").remove();
+}
+
+
+
+// Render all HTML and styles associated with movie
+function returnAndRenderMovie(movie) {
+  removeFrontPage();
+  renderMoviePage(movie);
+  determineScoreBoxColor(movie);
+  watchSubmit();
+}
+
+// Render HTML and CSS around the result (Movie)
+function renderMoviePage(movie) {
   $("body").html(`
-    <div class="movie-info-wrapper">
-      ${result.id}
+    <header class="logo-banner" role="banner">
+      <img src="./img/reel.png" alt="Filmtelligence logo">
+    </header>
+    <div class="search-row">
+      <div class="container">
+        <form class="js-search">
+          <input type="text" class="js-query wide" placeholder="Search Movie">
+        </form>
+      </div>
     </div>
+    <main role="main">
+      <div class="movie-info-wrapper">
+      </div>
+    </main>
   `)
-  $(".movie-info-wrapper").css("background-image", `url("https://image.tmdb.org/t/p/w500${result.backdrop_path}")`)
+
+  $(".movie-info-wrapper").css("background-image", `url("https://image.tmdb.org/t/p/w500${movie.backdrop_path}")`)
   $(".movie-info-wrapper").append(`<div class="movie-wrapper-overlay"></div>`)
 
   $(".movie-wrapper-overlay").append(`
     <div class="container">
       <div class="movie-header-left">
-        <img class="poster" src="https://image.tmdb.org/t/p/w500${result.poster_path}" alt="${result.original_title} Poster">
+        <img class="poster" src="https://image.tmdb.org/t/p/w500${movie.poster_path}" alt="${movie.original_title} Poster">
       </div>
       <div class="movie-header-right">
-        <h1 class="movie-title"> ${result.original_title} <span class="movie-year">(${result.release_date.substring(0, 4)})</span> </h1>
-        <p class="movie-overview">${(result.overview.length > 300) ? (result.overview.substring(0, 280) + "...") : result.overview }</p>
+        <h1 class="movie-title"> ${movie.original_title} <span class="movie-year">(${movie.release_date.substring(0, 4)})</span> </h1>
+        <p class="movie-overview">${movie.overview}</p>
         <hr/>
         <div class="movie-reception-left">
           <h2 class="movie-reception">Reception</h2>
-          <p class="movie-reception-text">Based on ${result.vote_count} votes.</p>
+          <p class="movie-reception-text">Based on ${movie.vote_count} votes.</p>
         </div>
         <div class="movie-reception-right">
           <div class="score-box">
-            <p class="movie-reception-average-score">${result.vote_average}</p>
+            <p class="movie-reception-average-score">${movie.vote_average}</p>
           </div>
         </div>
         <hr/>
       </div>
     </div>
   `)
+}
 
-  if (result.vote_average >= 7) {
+function determineScoreBoxColor(movie) {
+  if (movie.vote_average >= 7) {
     $(".score-box").css("background-color", "#55efc4");
-  } else if (result.vote_average >= 4) {
+  } else if (movie.vote_average >= 4) {
     $(".score-box").css("background-color", "#fdcb6e");
-  } else if (result.vote_average >= 0) {
+  } else if (movie.vote_average >= 0) {
     $(".score-box").css("background-color", "#d63031");
   }
-
-
-
 }
+
+
 
 function initializeApp() {
   renderFrontPage();
